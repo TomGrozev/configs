dd67769a66b1a0efcfc1ef30b7bf010eb00d21c1
Tom <enquire@tomgrozev.com>
2020-11-02 01:00:37 +1100

    Jetbrains setup

--- setup.sh
+++ setup.sh
@@ -64,7 +64,7 @@ printf "Plugins installed\n\n"
 # Setup git environment
 read -p "Setup git? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    printf "~~> Seting git environment\n"
+    printf "\n~~> Seting git environment\n"
     read -p "Enter your name: " git_name
     read -p "Enter your email: " git_email
 
@@ -83,7 +83,7 @@ printf "\n\n"
 # Setup SSH
 read -p "Setup SSH Keys? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    printf "~~> Setting up SSH\n"
+    printf "\n~~> Setting up SSH\n"
 
     SSH_KEY_PATH="$HOME/.ssh"
     if [ -e $SSH_KEY_PATH/id_rsa.pub ]; then
@@ -97,6 +97,52 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
     xclip -selection clipboard < $SSH_KEY_PATH/id_rsa.pub
     printf "Public SSH Key copied to clipboard. Add it to github here is how: https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account.\n"
+fi
+
+printf "\n\n"
+
+
+# Setup Jetbrains
+read -p "Setup Jetbrains Toolbox? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    printf "\n~~> Setting Jetbrains toolbox\n"
+
+    read -p "Where to install Jetbrains? (e.g. /opt/jetbrains)? " toolbox_path
+    toolbox_path=${toolbox_path:-/opt/jetbrains}
+
+    if [ ! "$(ls -A ${HOME}/.local/share/JetBrains/Toolbox 2> /dev/null)" ]; then
+        printf "Installing JetBrains Toolbox"
+
+        TOOLBOX_URL=$(curl --silent 'https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release' \
+        -H 'Origin: https://www.jetbrains.com' \
+        -H 'Accept-Encoding: gzip, deflate, br' \
+        -H 'Accept-Language: en-US,en;q=0.8' \
+        -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+        -H 'Referer: https://www.jetbrains.com/toolbox/download/' \
+        -H 'Connection: keep-alive' \
+        -H 'DNT: 1' \
+        --compressed \
+      | grep -Po '"linux":.*?[^\\]",' \
+      | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
+
+        install -d ${HOME}/bin
+        curl -sL ${TOOLBOX_URL} | tar xvz --directory=${HOME}/bin --strip-components=1
+
+        $(jetbrains-toolbox)
+    fi
+
+    #mkdir -p $toolbox_path/toolbox
+    sudo mkdir -p $toolbox_path/lib
+    sudo mkdir -p $toolbox_path/bin
+
+    #if [ ! "$(ls -A $toolbox_path/toolbox)" ]; then
+    #    cp -r ${HOME}/.local/share/JetBrains/Toolbox $toolbox_path/toolbox
+    #    rm -rf ${HOME}/.local/share/JetBrains/Toolbox
+    #    ln -s $toolbox_path/toolbox ${HOME}/.local/share/JetBrains/Toolbox
+    #fi
+
+    printf "Set install location to '$toolbox_path/lib' and the shell scripts location to '$toolbox_path/bin'"
+
 fi
 
 printf "\n\n"
