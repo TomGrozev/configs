0f4da49f22a4563016347954e5bfcccffc203219
TomGrozev <TomGrozev@users.noreply.github.com>
2020-12-23 11:17:43 +1100

    Elixir and script changes

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# Packages to install
+PACKAGES="python3 python3-pip python3-setuptools xclip zsh neovim thefuck autojump bat curl gcc make cmake autoconf automake python3-docutils nodejs npm golang default-jdk"
+DNF_PACKAGES="$PACKAGES python3-devel pkgconfig libseccomp-devel jansson-devel libyaml-devel libxml2-devel gcc-c++"
+DEBIAN_PACKAGES="$PACKAGES python3-dev pkg-config libseccomp-dev libjansson-dev libyaml-dev libxml2-dev build-essential"
+
 ## Any setup commands on new system
 
 # Get distro
 DISTRO="$(awk -F= '/^ID=/{print $2}' /etc/os-release)"
+printf "You are using $DISTRO\n\n"
 
 # Set install command
 INSTALL_CMD=$(case "$DISTRO" in
-    ("fedora") printf "dnf install" ;;
-    ("debian") printf "apt install" ;;
-    ("ubuntu") printf "apt install" ;;
+    ("fedora") printf "dnf install -y $DNF_PACKAGES" ;;
+    ("debian") printf "apt install -y $DEBIAN_PACKAGES" ;;
+    ("ubuntu") printf "apt install -y $DEBIAN_PACKAGES" ;;
 esac)
 
 if [ "$INSTALL_CMD" == "" ]; then
@@ -17,17 +23,17 @@ if [ "$INSTALL_CMD" == "" ]; then
     exit 1
 fi
 
-printf "You are using $DISTRO\n\n"
-
 INSTALL_CMD="sudo $INSTALL_CMD"
 
 # Install some useful packages
-PACKAGES="python3 python3-pip python3-dev python3-setuptools xclip zsh neovim thefuck autojump bat curl"
-
 printf "~~> Installing packages\n"
-$INSTALL_CMD $PACKAGES
+$INSTALL_CMD
+printf "Packages installed\n\n"
 
 printf "\n\n"
+
+# Make sure in home dir
+cd $HOME
 
 # Set ZSH as shell
 printf "~~> Setting ZSH as default shell\n"
@@ -44,6 +50,22 @@ printf "~~> Installing fonts\n"
 fc-cache -fv &> /dev/null
 printf "Fonts installed\n\n"
 
+# Install ctags
+if [[ -f "/usr/local/bin/ctags" ]]; then
+    printf "[[ Ctags already installed ]]\n"
+else
+    printf "~~> Installing Ctags\n"
+    git clone https://github.com/universal-ctags/ctags /tmp/ctags &> /dev/null
+    cd /tmp/ctags
+    sh ./autogen.sh &> /dev/null
+    sh ./configure --prefix=/usr/local &> /dev/null
+    make &> /dev/null
+    sudo make install &> /dev/null
+    rm -rf /tmp/ctags
+    cd $HOME
+    printf "Ctags installed\n\n"
+fi
+
 # Install submodules
 printf "~~> Installing Modules\n"
 git submodule init
@@ -51,16 +73,29 @@ git submodule update
 printf "Modules Installed\n\n"
 
 # Install themes
-printf "~~> Installing Themes\n"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &> /dev/null
-printf "Themes installed\n\n"
+if [[ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]]; then
+    printf "[[ Themes already installed ]]\n"
+else
+    printf "~~> Installing Themes\n"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &> /dev/null
+    printf "Themes installed\n\n"
+fi
 
 # Install plugins
-printf "~~> Installing Plugins\n"
+printf "~~> Installing Shell Plugins\n"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &> /dev/null
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &> /dev/null
-printf "Plugins installed\n\n"
+printf "Shell Plugins installed\n\n"
 
+# Update vim plugins
+printf "~~> Installing Vim Plugins\n"
+python3 $HOME/.vim_runtime/update_plugins.py
+printf "Vim Plugins installed\n\n"
+
+# Setup youcompleteme
+printf "~~> Installing YouCompleteMe\n"
+python3 $HOME/.vim_runtime/plugins/YouCompleteMe/install.py --go-completer --java-completer --quiet
+printf "YouCompleteMe installed\n\n"
 
 # Setup git environment
 read -p "Setup git? " -n 1 -r
@@ -144,6 +179,49 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
     printf "Set install location to '$toolbox_path/lib' and the shell scripts location to '$toolbox_path/bin'"
 
+fi
+
+printf "\n\n"
+
+printf "~> Language specifics\n"
+
+# Setup ELixir Environment
+read -p "Setup Elixir Environment? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    printf "\n~~> Setting up Elixir Environment\n"
+    # Check if elixir already installed
+    if ! command -v elixir &> /dev/null; then
+        # Install elixir
+        function debian_elixir_install {
+            wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb -P /tmp/ && sudo dpkg -i /tmp/erlang-solutions_2.0_all.deb
+            rm /tmp/erlang-solutions_2.0_all.deb
+            sudo apt-get update
+            sudo apt-get install esl-erlang
+            sudo apt-get install elixir
+        }
+
+        case "$DISTRO" in
+            ("fedora") sudo dnf install -y elixir ;;
+            ("debian") debian_elixir_install ;;
+            ("ubuntu") debian_elixir_install  ;;
+        esac
+    fi
+
+    # Install Hex
+    mix local.hex
+
+    # Setup Phoenix
+    mix archive.install hex phx_new
+
+    # Setup Elixir LS
+    git clone https://github.com/elixir-lsp/elixir-ls $HOME/.local/share/elixir-ls &> /dev/null
+    cd $HOME/.local/share/elixir-ls
+    mix deps.get
+    mix compile
+    MIX_ENV=prod mix elixir_ls.release
+    cd $HOME
+
+    printf "Elixir Environment setup\n\n"
 fi
 
 printf "\n\n"
